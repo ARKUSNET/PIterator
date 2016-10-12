b0f278a08a683af0a21ecc4058817407c6d89690
Arthur Kuspakov <artur-6272@rambler.ru>
2016-10-12 09:58:07 +0300

    Pattern Iterator

--- src/pattern_iterator/MenuTestDrive.java
+++ src/pattern_iterator/MenuTestDrive.java
@@ -1,5 +1,7 @@
 package pattern_iterator;
 
+import java.util.ArrayList;
+
 /**
  * Created by a.kuspakov on 11.10.2016.
  */
@@ -8,8 +10,12 @@ public class MenuTestDrive {
         PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
         DinerMenu dinerMenu = new DinerMenu();
         CafeMenu cafeMenu = new CafeMenu();
+        ArrayList<Menu> menus = new ArrayList<Menu>();
+        menus.add(0, pancakeHouseMenu);
+        menus.add(1, dinerMenu);
+        menus.add(2, cafeMenu);
 
-        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu, cafeMenu);
+        Waitress waitress = new Waitress(menus);
 
         waitress.printMenu();
     }
--- src/pattern_iterator/Waitress.java
+++ src/pattern_iterator/Waitress.java
@@ -1,30 +1,29 @@
 package pattern_iterator;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
  * Created by a.kuspakov on 11.10.2016.
  */
 public class Waitress {
-    Menu pancakeHouseMenu;
-    Menu dinerMenu;
-    Menu cafeMenu;
+    ArrayList menus;
+    String[] eatTime = {"\nBREAKFAST", "\nLUNCH", "\nDINNER"};
+    int eatFlag = 0;
 
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu){
-        this.pancakeHouseMenu = pancakeHouseMenu;
-        this.dinerMenu = dinerMenu;
-        this.cafeMenu = cafeMenu;
+    public Waitress(ArrayList menus){
+        this.menus = menus;
     }
 
     public void printMenu(){
-        Iterator pancakeIterator = pancakeHouseMenu.createIterator();
-        Iterator dinerIterator = dinerMenu.createIterator();
-        Iterator cafeIterator = cafeMenu.createIterator();
-        System.out.println("MENU\n---\nBREAKFAST");
-        printMenu(pancakeIterator);
-        System.out.println("\nLUNCH");
-        printMenu(dinerIterator);
-        System.out.println("\nDINNER");
-        printMenu(cafeIterator);
+        Iterator menuIterator = menus.iterator();
+        System.out.println("MENU");
+        while (menuIterator.hasNext())
+        {
+            if(eatFlag < 4) System.out.println(eatTime[eatFlag]);
+            eatFlag++;
+            Menu menu = (Menu)menuIterator.next();
+            printMenu(menu.createIterator());
+        }
     }
 
     private void printMenu(Iterator iterator){
